e889dde2eed4818f9673442b1248b9ab8b6e0c66
Emmanuel Eshofonie <ejieshofonie@yahoo.com>
2021-06-24 21:07:01 +0100

    Add files via upload

---
+++ conn.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: "localhost",
+  port:3306,
+  user: "root",
+  password: ""
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
